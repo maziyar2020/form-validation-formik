d9a01241977ee10525d5bb3f32a0154d41db3901
maziyar <maziroadtodivine@gmail.com>
2023-08-26 09:09:56 +0330

    Form controll with yup.

--- src/components/SingUpForm.jsx
+++ src/components/SingUpForm.jsx
@@ -1,5 +1,6 @@
 'use client';
 import { useFormik } from 'formik'
+import * as Yup from 'yup'
 
 
 const SingUpForm = () => {
@@ -7,31 +8,37 @@ const SingUpForm = () => {
     const initialValues = {
         name: '',
         email: '',
-        password: ''
+        phoneNumber: '',
+        password: '',
+        passwordConfirm: ''
     }
 
     const onSubmit = (values) => console.log(values)
 
-    const validate = (values) => {
-        let errors = {}
-
-        if (!values.name) {
-            errors.name = "Name is required"
-        }
-        if (!values.email) {
-            errors.email = "email is Required"
-        }
-        if (!values.password) {
-            errors.password = "password is required"
-        }
-        return errors
-    }
+    const validationSchema = Yup.object({
+        // schema for name
+        name: Yup.string()
+            .required('این فیلد الزامیست')
+            .min(6, 'باید طولانی تر از 6 حرف باشد')
+            .max(20, 'نباید بیش از 20 کاراکتر وارد کنید'),
+        // schema for email
+        email: Yup.string().email('ایمیل صحیح وارد نشده').required('این فیلد الزامیست'),
+        // schema for phonenumber
+        phoneNumber: Yup.string().required('این فیلد الزامیست')
+            .matches(/^[0-9]{11}$/, 'شماره تلفن نامعتبر است'),
+        // schema for pw
+        password: Yup.string().required('این فیلد الزامیست'),
+        passwordConfirm: Yup.string().required('این فیلد الزامی است').oneOf(
+            [Yup.ref('password'), null], "پسورد مطابقت ندارد"
+        )
+    })
 
 
     const formik = useFormik({
         initialValues,
         onSubmit,
-        validate
+        validationSchema,
+        validateOnMount: true
     })
 
 
@@ -39,15 +46,14 @@ const SingUpForm = () => {
     return (
         <div>
             <form className="bg-gray-100 p-8 rounded-xl" onSubmit={formik.handleSubmit}>
+                {/* name field */}
                 <div className="form-control">
                     <label htmlFor="name">name</label>
                     <input
                         type="text"
                         id='name'
                         name="name"
-                        value={formik.values.name}
-                        onChange={formik.handleChange}
-                        onBlur={formik.handleBlur}
+                        {...formik.getFieldProps('name')}
                     />
                     {
                         formik.errors.name && formik.touched.name &&
@@ -56,15 +62,14 @@ const SingUpForm = () => {
                         </div>
                     }
                 </div>
+                {/* email field */}
                 <div className="form-control">
                     <label htmlFor="email">email</label>
                     <input
                         type="text"
                         id='email'
                         name="email"
-                        value={formik.values.email}
-                        onChange={formik.handleChange}
-                        onBlur={formik.handleBlur}
+                        {...formik.getFieldProps('email')}
                     />
                     {
                         formik.errors.email && formik.touched.email &&
@@ -73,26 +78,59 @@ const SingUpForm = () => {
                         </div>
                     }
                 </div>
+                {/* phone number field */}
+                <div className="form-control">
+                    <label htmlFor="phoneNumber">phone Number</label>
+                    <input
+                        type="text"
+                        id='phoneNumber'
+                        name="phoneNumber"
+                        {...formik.getFieldProps('phoneNumber')}
+                    />
+                    {
+                        formik.errors.phoneNumber && formik.touched.phoneNumber &&
+                        <div className="text-red-500">
+                            {formik.errors.phoneNumber}
+                        </div>
+                    }
+                </div>
+                {/* password field */}
                 <div className="form-control">
                     <label htmlFor="password">password</label>
                     <input
                         type="text"
                         id='password'
                         name="password"
-                        value={formik.values.password}
-                        onChange={formik.handleChange}
-                        onBlur={formik.handleBlur}
+                        {...formik.getFieldProps('password')}
                     />
                     {
                         formik.errors.password && formik.touched.password &&
-                        <div div className="text-red-500">
+                        <div className="text-red-500">
                             {formik.errors.password}
+                        </div>
+                    }
+                </div>
+                {/* password Confirm */}
+                <div className="form-control">
+                    <label htmlFor="password">password Confirm</label>
+                    <input
+                        type="text"
+                        id='passwordConfirm'
+                        name="passwordConfirm"
+                        {...formik.getFieldProps('passwordConfirm')}
+                    />
+                    {
+                        formik.errors.passwordConfirm && formik.touched.passwordConfirm &&
+                        <div className="text-red-500">
+                            {formik.errors.passwordConfirm}
                         </div>
                     }
                 </div>
                 <button
                     className="bg-violet-500 rounded-md text-white"
-                    type='submit'>Submit
+                    type='submit'
+                    disabled={!formik.isValid}
+                >Submit
                     </button>
             </form>
         </div >
